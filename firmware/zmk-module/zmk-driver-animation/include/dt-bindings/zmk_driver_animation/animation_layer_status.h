c00f4b23f030103a9611846f6efd94437b5e9056
cormoran <cormoran707@gmail.com>
2025-05-17 17:16:42 +0900

    Remove zmk-driver-animation & zmk-driver-ext-power-transient to convert to submodule

--- firmware/zmk-module/zmk-driver-animation/include/dt-bindings/zmk_driver_animation/animation_layer_status.h
+++
@@ -1,7 +0,0 @@
-/*
- * Copyright (c) 2025 cormoran
- *
- * SPDX-License-Identifier: MIT
- */
-
-#define ANIMATION_LAYER_STATUS_CMD_FOR_PERIPHERAL 0
--- firmware/zmk-module/zmk-driver-animation/run_test.sh
+++
@@ -1,6 +0,0 @@
-REPO_ROOT=$(git rev-parse --show-superproject-working-tree --show-toplevel | head -1)
-BUILD_DIR=$REPO_ROOT/firmware/zmk-module/build
-
-ZMK_EXTRA_MODULES="$REPO_ROOT/firmware/zmk-module/$(basename $(dirname $0))"
-cd $REPO_ROOT/firmware/zmk_firmware/app
-west build -d ${BUILD_DIR}/test --pristine --board native_posix_64 -- -DZMK_EXTRA_MODULES="$ZMK_EXTRA_MODULES"
